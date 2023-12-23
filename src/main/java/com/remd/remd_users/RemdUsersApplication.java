c9f6ddb83bbcda43cd605cfac3c34e8a533b9f24
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-12-23 09:33:49 +0100

    Initial commit

---
+++ src/main/java/com/remd/remd_users/RemdUsersApplication.java
@@ -0,0 +1,13 @@
+package com.remd.remd_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemdUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemdUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remd/remd_users/RemdUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remd.remd_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemdUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
